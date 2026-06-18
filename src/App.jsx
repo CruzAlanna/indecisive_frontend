f7079a48782b5e964d4bb2346522b0e2f1e434c8
CruzAlanna <lannikcru@gmail.com>
2026-06-19 09:53:12 +1000

    Configure frontend for production API

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import './App.css'
 import Header from './Header'
 import { Outlet } from 'react-router-dom';
 import Footer from './Footer'
+import axios from 'axios';
+
+axios.defaults.baseURL = process.env.NODE_ENV === "development" 
+  ? "http://localhost:3000" 
+  : "https://indecisive-app.onrender.com";
 
 function App() {
   return (
