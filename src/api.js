2e9051b1f04d3942c3553ad491a04aa7efe9dfb3
CruzAlanna <lannikcru@gmail.com>
2026-06-19 10:23:10 +1000

    Bug fix

--- src/api.js
+++ src/api.js
@@ -1,3 +1,5 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000';
+const API_BASE_URL = import.meta.env.MODE === "development" 
+? "http://localhost:3000" 
+: "https://indecisive-app.onrender.com";
 
 export const apiUrl = (path) => `${API_BASE_URL}${path}`;
