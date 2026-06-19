b9ddd109af2213f8f3457d7445a8cdd42d29a4d8
CruzAlanna <lannikcru@gmail.com>
2026-06-19 10:30:54 +1000

    Bug fix

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = import.meta.env.MODE === "development" 
+export const API_BASE_URL = import.meta.env.MODE === "development" 
 ? "http://localhost:3000" 
 : "https://indecisive-app.onrender.com";
 
